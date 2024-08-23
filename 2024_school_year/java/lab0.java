03b266467c90c6cfa0395c8e24ce60e897d68c7a
nothingisrealffs <88836848+nothingisrealffs@users.noreply.github.com>
2024-08-23 13:39:42 -0400

    today's changes
    Did today's lab, and tightened up the hello world.

---
+++ 2024_school_year/java/lab0.java
@@ -0,0 +1,7 @@
+public class lab0 {
+    public static void main(String[] args) {
+        System.out.println("Hi. My name is Michael.");
+        System.out.println("My major is CIT and Informatics.");
+        System.out.println("My favorite programming language is Rust");
+    }    
+}
