9b860b643042c325a57ff2dee02540a784a2165f
nothingisrealffs <88836848+nothingisrealffs@users.noreply.github.com>
2024-09-17 19:49:52 -0400

    basic java add
    school stuff

--- 2024_school_year/java/Manager.java
+++
@@ -1,8 +0,0 @@
-public class Manager {
-    private double bonus;
-    public Manager(){
-        bonus = 0;
-    }
-    public double getBonus(){return bonus;}
-    public void setBonus(double bonus){thisbonus = bonus;}
-}
